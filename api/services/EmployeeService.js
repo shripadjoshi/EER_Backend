02a1bd843d1519caa69a2614730b93490af14647
Shripad <shripad.joshi.s@gmail.com.com>
2016-10-03 12:59:54 +0530

    Employee service changes

--- api/services/EmployeeService.js
+++ api/services/EmployeeService.js
@@ -1,7 +1,7 @@
 module.exports = { 
 
 	getSelectedEmployees: function(next) {
-        Employee.query('SELECT full_name from Employee where Employee.id not in(select employee from USER)', 
+        Employee.query('SELECT id,full_name from Employee where Employee.id not in(select employee from USER) and Employee.IsDeleted = 0', 
         	function(err, employees){
         	if (err) throw err;
         	next(employees);
